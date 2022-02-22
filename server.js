e461935ac00842aa17cdbf59d1a0f224809e1f99
vanilanorca <vilopesalves@hotmail.com>
2022-02-22 02:41:46 -0300

    .

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/synvia-pokedex"));
+app.use(express.static("./dist/synvia-pokedex"));
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/synvia-pokedex/index.html"));
+  res.sendFile('index.html', {root: 'synvia-pokedex'})
 });
 app.listen(process.env.PORT || 8080);
